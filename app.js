41ee5185e63f25b4501232592060df1b8c3eb83d
Oliver <hardwiog@mail.uc.edu>
2015-02-04 13:43:46 -0500

    First commit
    Unfortunately posting this site online would cost too much. So I have
    created a public repo for anyone to view my node.js!

---
+++ app.js
@@ -0,0 +1,30 @@
+// require express
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+// utilize routes folder
+var routes = require('./routes');
+
+// Set view engine for app
+app.set('view engine', 'ejs'); 
+app.use("/styles", express.static(__dirname + "/styles"));
+app.use("/images", express.static(__dirname + "/images"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.locals.appdata = require('./data.json');
+
+// Set pages
+app.get('/', routes.index);
+app.get('/about', routes.about);
+app.get('/dev', routes.dev);
+app.get('/login', routes.login);
+app.get('*', routes.wrong);
+
+var server = app.listen(3000, function() {
+	console.log("listening on port 3000");
+});
+
+
