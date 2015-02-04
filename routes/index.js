b57a63d2a654b109d50b41f4df99afc178fd9aa6
Oliver <hardwiog@users.noreply.github.com>
2015-02-04 14:40:14 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,33 @@ exports.index = function(req, res) {
 
 }
 
+// Set about route
+exports.about = function(req, res) {
+	res.render('about', {
+		title: 'About Us',
+		classname: 'about'
+	});
+
+}
+
+// Set info route
+exports.dev = function(req, res) {
+	res.render('dev', {
+		title: 'Showcase',
+		classname: 'app'
+	});
+
+}
+
+// Set login route
+exports.login = function(req, res) {
+	res.render('login', {
+		title: 'Login',
+		classname: 'login'
+	});
+
+}
+
 // Set error page
 exports.wrong = function(req, res) {
 	res.send('Page not found: 404');
