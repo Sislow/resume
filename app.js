42c9326d7abfa0cb625445e0b696da4cc36d1010
Oliver <hardwiog@users.noreply.github.com>
2015-02-04 14:49:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // initialize
 var express = require('express'), http = require('http');
 var bodyParser = require('body-parser');
+var path = require('path');
 var port = process.env.PORT || 3000;
 var app = express();
 
 // utilize routes folder
 var routes = require('./routes');
+//app.set('routes', path.join(__dirname, 'routes'));
 
 // Set view engine for app
 app.set('view engine', 'ejs'); 
@@ -18,12 +20,13 @@ app.locals.appdata = require('./data.json');
 
 // Set pages
 app.get('/', routes.index);
-app.get('/about', routes.about);
-app.get('/dev', routes.dev);
-app.get('/login', routes.login);
+app.get('/about', function(req, res) {res.render('about');});
+app.get('/dev', function(req, res) {res.render('dev');});
+app.get('/login', function(req, res) {res.render('login');});
+
 app.get('*', routes.wrong);
 
 var server = http.createServer(app);
-app.listen(port);
 
+app.listen(port);
 
